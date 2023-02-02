3eb91b76e3e2b3cf45a18c60213f91f48fc2d6da
vjain25 <90483086+vjain25@users.noreply.github.com>
2023-02-01 23:55:10 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,139 @@
+"""
+CS/DS 549 Spring 2023 Programming and Model Training Assignment
+
+The goal is to define a better model and training hyperparameters to beat the minimum
+required evaluation/validation accuracy of 0.82  at the very least, but also to compete
+in the class challenge for best training results.
+
+Only edit code between the comments:
+#########################
+# Edit code here
+# vvvvvvvvvvvvvvvvvvvvvvv
+<code>
+# ^^^^^^^^^^^^^^^^^^^^^^^
+"""
+import wandb
+from wandb.keras import WandbMetricsLogger
+
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import tensorflow_datasets as tfds
+from matplotlib import pyplot as plt
+
+
+
+if __name__ == '__main__':
+
+    # Leave entity="bu-spark-ml" and project="hw1_spring2023"
+    # put your BU username in the `group=` parameter
+    wandb.init(
+        project="hw1_spring2023",  # Leave this as 'hw1_spring2023'
+        entity="bu-spark-ml",  # Leave this
+        group="vjain25",  # <<<<<<< Put your BU username here
+        notes="Minimal model"  # <<<<<<< You can put a short note here
+    )
+
+    """
+    Use tfds to load the CIFAR10 dataset and visualize the images and train.
+
+    The datasets used are:
+    https://www.tensorflow.org/datasets/catalog/cifar10
+    https://www.tensorflow.org/datasets/catalog/cifar10_corrupted
+
+    tfds.load() whill first check if the dataset is already downloaded to the
+    path in `data_dir`. If not, it will download the dataset to that path..
+    """
+    # Load the CIFAR10 dataset
+    print("Loading CIFAR10 dataset...")
+    (ds_cifar10_train, ds_cifar10_test), ds_cifar10_info = tfds.load(
+        'cifar10',
+        split=['train', 'test'],
+        data_dir='/projectnb/ds549/datasets/tensorflow_datasets',
+        shuffle_files=True, # load in random order
+        as_supervised=True, # Include labels
+        with_info=True, # Include info
+    )
+
+    # Optionally uncomment the next 3 lines to visualize random samples from each dataset
+    #fig_train = tfds.show_examples(ds_cifar10_train, ds_cifar10_info)
+    #fig_test = tfds.show_examples(ds_cifar10_test, ds_cifar10_info)
+    #plt.show()  # Display the plots
+
+    def normalize_img(image, label):
+        """Normalizes images: `uint8` -> `float32`."""
+        return tf.cast(image, tf.float32) / 255., label
+
+    # Prepare cifar10 training dataset
+    ds_cifar10_train = ds_cifar10_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_cifar10_train = ds_cifar10_train.cache()     # Cache data
+    ds_cifar10_train = ds_cifar10_train.shuffle(ds_cifar10_info.splits['train'].num_examples)
+    ds_cifar10_train = ds_cifar10_train.batch(32)  # <<<<< To change batch size, you have to change it here
+    ds_cifar10_train = ds_cifar10_train.prefetch(tf.data.AUTOTUNE)
+
+    # Prepare cifar10 test dataset
+    ds_cifar10_test = ds_cifar10_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_cifar10_test = ds_cifar10_test.batch(32)    # <<<<< To change batch size, you have to change it here
+    ds_cifar10_test = ds_cifar10_test.cache()
+    ds_cifar10_test = ds_cifar10_test.prefetch(tf.data.AUTOTUNE)
+
+    # Define the model here
+    model = tf.keras.models.Sequential([
+        keras.Input(shape=(32, 32, 3)),
+        #####################################
+        # Edit code here -- Update the model definition
+        # You will need a dense last layer with 10 output channels to classify the 10 classes
+        # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        
+        
+
+        layers.Conv2D(32, (3,3), input_shape = (32,32,3), padding = 'same'),
+        layers.Dropout(0.2),
+        layers.Conv2D(32, (2,2), padding = 'same'),
+        layers.MaxPooling2D(2, 2),
+        layers.Dropout(0.2),
+        layers.Conv2D(64, (2,2), padding = 'same'),
+        layers.MaxPooling2D(2, 2),
+        layers.Dropout(0.2),
+        layers.Conv2D(128, (3,3), padding = 'same'),
+        layers.MaxPooling2D(2, 2),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(1024, activation = 'relu'),
+        layers.Dropout(0.2),
+        layers.Dense(512, activation = 'relu'),
+        layers.Dropout(0.2),
+        layers.Dense(256, activation = 'relu'),
+        layers.Dropout(0.2),
+        layers.Dense(10, activation = 'softmax'),
+
+
+    ])
+
+    # Log the training hyper-parameters for WandB
+    # If you change these in model.compile() or model.fit(), be sure to update them here.
+    wandb.config = {
+        #####################################
+        # Edit these as desired
+        # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        "learning_rate": 0.001,
+        "optimizer": "adam",
+        "epochs": 20,
+        "batch_size": 32
+        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    }
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=.001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    history = model.fit(
+        ds_cifar10_train,
+        epochs=20,
+        validation_data=ds_cifar10_test,
+        callbacks=[WandbMetricsLogger()]
+    )
+
+    wandb.finish()
